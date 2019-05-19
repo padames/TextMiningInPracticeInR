a09d58d6099f8a1a5b5c27ee3950c4b243cb3ac6
padames <pablo.adames@gmail.com>
2019-05-19 12:34:09 -0600

    Starting script file to do analysis of Delta Airlines Customer Service Tweets

---
+++ DeltaAnalysis.R
@@ -0,0 +1,11 @@
+# Exercises from the book Text Mining In Practice In R
+# Chapter 2: Text Mining Basics
+# Data source: https://github.com/padames/text_mining/blob/master/oct_delta.csv
+
+# Set Options
+options(stringsAsFactors = FALSE)
+Sys.setlocale('LC_ALL', 'C')
+
+library(stringi)
+library(stringr)
+library(qdap)
