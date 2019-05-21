d6f55d7772411ef6bac3e92c4fffd1211578d175
padames <pablo.adames@gmail.com>
2019-05-20 23:25:58 -0600

    Added helper functions and created and cleaned up the corpus of text in preparation for bag of words analysis.

--- DeltaAnalysis.R
+++ DeltaAnalysis.R
@@ -10,7 +10,30 @@ library(stringi)
 library(stringr)
 library(qdap)
 library(curl)
+library(tm)
 
-text.df2 <- read.csv(curl("https://raw.githubusercontent.com/padames/text_mining/master/oct_delta.csv"), header = T, stringsAsFactors = F)
+# STEP 0: read the csv file (already cleand up by Ted Kwartler)
+# https://stackoverflow.com/a/31178716/1585486
+text.df <- read.csv(curl("https://raw.githubusercontent.com/padames/text_mining/master/oct_delta.csv"), header = T, stringsAsFactors = F)
+# > str(text.df)
+# 'data.frame':	1377 obs. of  5 variables:
+#   $ weekday: chr  "Thu" "Thu" "Thu" "Thu" ...
+# $ month  : chr  "Oct" "Oct" "Oct" "Oct" ...
+# $ date   : int  1 1 1 1 1 1 1 1 1 1 ...
+# $ year   : int  2015 2015 2015 2015 2015 2015 2015 2015 2015 2015 ...
+# $ text   : chr  "@mjdout I know that can be frustrating..we hope to have you parked and deplaned shortly. Thanks for your patience.  *AA" ...
+
+df <- data.frame(doc_id = seq(1:nrow(text.df)), # a numeric index
+                 text = text.df$text) # the vector of 1377 tweets
+
+# STEP 1: Create a corpus
+
+corpus <- VCorpus(DataframeSource(df))
+
+# STEP 2: Clean up the corpus
+
+source("Helpers.R")
+
+corpus <- clean.corpus(corpus)
 
 
