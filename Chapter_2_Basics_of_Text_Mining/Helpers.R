ef7c53388464c1a2ca8c35554402566f7b7ec91d
padames <pablo.adames@gmail.com>
2019-06-05 00:08:58 -0600

    Renamed chapter 2 folder to avoid url whitespace encoding

---
+++ Chapter_2_Basics_of_Text_Mining/Helpers.R
@@ -0,0 +1,28 @@
+# Exercises from the book Text Mining In Practice In R
+# Chapter 2: Text Mining Basics
+# Helpers for main script
+library(magrittr)
+library(tm)
+
+tryTolower <- function(s) {
+  s_lowered <- tryCatch( tolower(s),
+                         error = function(e) e )
+  if (inherits(s_lowered, 'error')) {
+    s_lowered <- NA
+  }
+  return(s_lowered)
+}
+
+
+
+custom.stopwords <- c(stopwords("en"), 'lol', 'smh', 'delta')
+
+clean.corpus <- function(corp) {
+  corp <- corp %>%
+    tm_map(content_transformer(tryTolower)) %>%
+    tm_map(removeWords, custom.stopwords) %>%
+    tm_map(removePunctuation) %>%
+    tm_map(stripWhitespace) %>%
+    tm_map(removeNumbers)
+  return(corp)
+}
---
+++ Chapter_2_Basics_of_Text_Mining/README.md
@@ -0,0 +1,8 @@
+# Basics of Text Mining
+Exercises from Chapter Two of Ted Kwartler's book **Text Mining In Practice with R**
+
+## Reproducible exercise
+
+Using R Markdown to produce an HTML output file. 
+
+The result can be seen by pointing a browser to https://htmlpreview.github.io/?https://github.com/padames/TextMiningInPracticeInR/blob/master/Chapter%202%20Basics%20of%20Text%20Mining/Delta_Analysis.html
